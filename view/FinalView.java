c91635ee8ec57ce227639802573d995253d50837
Garik <ee.eeee.2000@mail.ru>
2024-07-06 01:12:20 +0400

    исправление

---
+++ view/FinalView.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.util.Scanner;
+
+public class FinalView {
+
+    public void start() throws Exception {
+        System.out.println("Want to create a student or teacher?");
+        System.out.println("Student - press 1");
+        System.out.println("Teacher - press 2");
+        System.out.println("If you have an account and would like to join the study group - press 3");
+        Scanner scanner = new Scanner(System.in);
+
+        switch (scanner.nextInt()) {
+            case 1 -> new StudentView().start();
+            case 2 -> new TeacherView().start();
+            case 3 -> new StudyGroupView().startView();
+
+            default -> System.out.println("Please enter a valid option");
+
+        }
+    }
+
+}
